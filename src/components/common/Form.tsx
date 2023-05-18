3682c210816c1afc2633dcb088a323ee2308695c
Jordy <jbonn2002@gmail.com>
2023-05-17 19:42:27 -0700

    finishing create property section

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -148,6 +148,73 @@ const Form = ({
               />
             </FormControl>
           </Stack>
+          <FormControl>
+            <FormHelperText
+              sx={{
+                fontWeight: 500,
+                margin: "10px 0",
+                fontSize: 16,
+                color: "#11142d",
+              }}
+            >
+              Enter Location
+            </FormHelperText>
+            <TextField
+              fullWidth
+              required
+              id="outlined-basic"
+              color="info"
+              variant="outlined"
+              {...register("location", {
+                required: true,
+              })}
+            />
+          </FormControl>
+          <Stack direction="column" gap={1} justifyContent="center" mb={2}>
+            <Stack direction="row" gap={2}>
+              <Typography
+                color="#11142d"
+                fontSize={16}
+                fontWeight={500}
+                my="10px"
+              >
+                Property Photo
+              </Typography>
+              <Button
+                component="label"
+                sx={{
+                  width: "fit-content",
+                  color: "#2ed480",
+                  textTransform: "capitalize",
+                  fontSize: 16,
+                }}
+              >
+                Upload *
+                <input
+                  hidden
+                  accept="image/*"
+                  type="file"
+                  onChange={(e) => {
+                    //@ts-ignore
+                    handleImageChange(e.target.files[0]);
+                  }}
+                />
+              </Button>
+            </Stack>
+            <Typography
+              fontSize={14}
+              color="#808191"
+              sx={{ wordBreak: "break-all" }}
+            >
+              {propertyImage?.name}
+            </Typography>
+          </Stack>
+          <CustomButton
+            type="submit"
+            title={formLoading ? "Submitting..." : "Submit"}
+            backgroundColor="#475be8"
+            color="#fcfcfc"
+          />
         </form>
       </Box>
     </Box>
